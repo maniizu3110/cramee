ae99b240361a8c40e2b276e0a04bf1d0be486394
maniizu3110 <maniizu3110@gmail.com>
2022-01-20 15:39:03 +0900

    データベースの制約に合わせてsqlファイル作成

--- server/models/student.go
+++ server/models/student.go
@@ -2,16 +2,18 @@ package models
 
 import "time"
 
+//go:generate go run ../codegen/main.go -file ${GOFILE} -dest ..
 type Student struct {
 	Model
-	FirstName         string    `json:"first_name"`
-	FirstNameKana     string    `json:"first_name_kana"`
-	LastName          string    `json:"last_name"`
-	LastNameKana      string    `json:"last_name_kana"`
-	PhoneNumber       string    `json:"phone_number"`
-	Email             string    `json:"email"`
-	Address           string    `json:"address"`
-	HashedPassword    string    `json:"hashed_password"`
-	Image             string    `json:"image"`
-	PasswordChangedAt time.Time `json:"password_changed_at"`
+	FirstName               string                   `json:"first_name"`
+	FirstNameKana           string                   `json:"first_name_kana"`
+	LastName                string                   `json:"last_name"`
+	LastNameKana            string                   `json:"last_name_kana"`
+	PhoneNumber             string                   `json:"phone_number"`
+	Email                   string                   `json:"email"`
+	Address                 string                   `json:"address"`
+	HashedPassword          string                   `json:"hashed_password"`
+	Image                   string                   `json:"image"`
+	PasswordChangedAt       time.Time                `json:"password_changed_at"`
+	StudentLectureSchedules []StudentLectureSchedule `json:"student_lecture_schedules"`
 }
--- server/models/teacher.go
+++ server/models/teacher.go
@@ -2,15 +2,18 @@ package models
 
 import "time"
 
+//go:generate go run ../codegen/main.go -file ${GOFILE} -dest ..
 type Teacher struct {
-	FirstName         string    `json:"first_name"`
-	FirstNameKana     string    `json:"first_name_kana"`
-	LastName          string    `json:"last_name"`
-	LastNameKana      string    `json:"last_name_kana"`
-	PhoneNumber       string    `json:"phone_number"`
-	Email             string    `json:"email"`
-	Address           string    `json:"address"`
-	HashedPassword    string    `json:"hashed_password"`
-	Image             string    `json:"image"`
-	PasswordChangedAt time.Time `json:"password_changed_at"`
+	Model
+	FirstName               string                   `json:"first_name"`
+	FirstNameKana           string                   `json:"first_name_kana"`
+	LastName                string                   `json:"last_name"`
+	LastNameKana            string                   `json:"last_name_kana"`
+	PhoneNumber             string                   `json:"phone_number"`
+	Email                   string                   `json:"email"`
+	Address                 string                   `json:"address"`
+	HashedPassword          string                   `json:"hashed_password"`
+	Image                   string                   `json:"image"`
+	PasswordChangedAt       time.Time                `json:"password_changed_at"`
+	TeacherLectureSchedules []TeacherLectureSchedule `json:"teacher_lecture_schedules"`
 }
