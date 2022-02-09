7cc45a66f9167e1736a573a566e2c0f057553504
maniizu3110 <maniizu3110@gmail.com>
2022-02-09 20:54:30 +0900

    gormでunique追加

--- api/models/student.go
+++ api/models/student.go
@@ -9,10 +9,10 @@ type Student struct {
 	FirstNameKana           string                   `json:"first_name_kana"`
 	LastName                string                   `json:"last_name"`
 	LastNameKana            string                   `json:"last_name_kana"`
-	PhoneNumber             string                   `json:"phone_number"`
-	Email                   string                   `json:"email"`
+	PhoneNumber             string                   `json:"phone_number" gorm:"unique"`
+	Email                   string                   `json:"email" gorm:"unique"`
 	Address                 string                   `json:"address"`
-	HashedPassword          string                   `json:"hashed_password"`
+	HashedPassword          string                   `json:"hashed_password" gorm:"unique"`
 	Image                   string                   `json:"image"`
 	PasswordChangedAt       time.Time                `json:"password_changed_at"`
 	StudentLectureSchedules []StudentLectureSchedule `json:"student_lecture_schedules"`
