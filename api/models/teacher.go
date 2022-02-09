8a878c09e3f7236824c212fa6771a2e18eb3b448
maniizu3110 <maniizu3110@gmail.com>
2022-02-09 21:06:37 +0900

    unique追加

--- api/models/teacher.go
+++ api/models/teacher.go
@@ -9,10 +9,10 @@ type Teacher struct {
 	FirstNameKana           string                   `json:"first_name_kana"`
 	LastName                string                   `json:"last_name"`
 	LastNameKana            string                   `json:"last_name_kana"`
-	PhoneNumber             string                   `json:"phone_number"`
-	Email                   string                   `json:"email"`
+	PhoneNumber             string                   `json:"phone_number" gorm:"unique"`
+	Email                   string                   `json:"email" gorm:"unique"`
 	Address                 string                   `json:"address"`
-	HashedPassword          string                   `json:"hashed_password"`
+	HashedPassword          string                   `json:"hashed_password" gorm:"unique"`
 	Image                   string                   `json:"image"`
 	PasswordChangedAt       time.Time                `json:"password_changed_at"`
 	TeacherLectureSchedules []TeacherLectureSchedule `json:"teacher_lecture_schedules"`
