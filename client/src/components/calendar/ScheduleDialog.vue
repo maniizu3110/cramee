22f4a2c0a0e37b7651b9eef5143b71804979e0ce
maniizu3110 <maniizu3110@gmail.com>
2022-02-14 15:54:59 +0900

    install timepicker

---
+++ client/src/components/calendar/ScheduleDialog.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <vue-timepicker></vue-timepicker>
+  </div>
+</template>
+<script>
+import VueTimepicker from "vue2-timepicker";
+export default {
+  name: "schedule-dialog",
+  component: {
+    VueTimepicker,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
