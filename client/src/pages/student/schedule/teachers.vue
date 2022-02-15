14a5bdfd5a188861a5843a505c698fe3d4cb4ef8
maniizu3110 <maniizu3110@gmail.com>
2022-02-15 09:38:06 +0900

    講師一覧取得

---
+++ client/src/pages/student/schedule/teachers.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-if="users">
+    <users-table :users="users" />
+  </div>
+</template>
+<script>
+import UsersTable from "../../../components/user/UsersTable.vue";
+
+export default {
+  components: {
+    UsersTable,
+  },
+  data() {
+    return {
+      users: [],
+    };
+  },
+  mounted() {
+    this.$axios.get("v1/teacher").then((res) => {
+      this.users = res.data.Data
+    });
+  },
+};
+</script>
