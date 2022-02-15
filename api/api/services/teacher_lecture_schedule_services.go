806849208dd7cf8db393ef736ee0915e6426e8e3
maniizu3110 <maniizu3110@gmail.com>
2022-02-15 11:39:06 +0900

    予約した時に生徒の予定も同時に登録

--- api/api/services/teacher_lecture_schedule_services.go
+++ api/api/services/teacher_lecture_schedule_services.go
@@ -1,6 +1,9 @@
 package services
 
-import "cramee/models"
+import (
+	"cramee/models"
+	"cramee/token"
+)
 
 //go:generate $GOPATH/bin/mockgen -source=$GOFILE -destination=${GOPACKAGE}_mock/${GOFILE}.mock.go -package=${GOPACKAGE}_mock
 
@@ -22,16 +25,21 @@ type TeacherLectureScheduleService interface {
 	SoftDelete(id uint) (*models.TeacherLectureSchedule, error)
 	HardDelete(id uint) (*models.TeacherLectureSchedule, error)
 	Restore(id uint) (*models.TeacherLectureSchedule, error)
+	UpdateWithStudentLectureSchedule(id uint, data *models.TeacherLectureSchedule) (*models.TeacherLectureSchedule, error)
 }
 
 type teacherLectureScheduleServiceImpl struct {
-	repo TeacherLectureScheduleRepository
+	token                      *token.Payload
+	repo                       TeacherLectureScheduleRepository
+	studentLectureScheduleRepo StudentLectureScheduleRepository
 	TeacherLectureScheduleService
 }
 
-func NewTeacherLectureScheduleService(repository TeacherLectureScheduleRepository) TeacherLectureScheduleService {
+func NewTeacherLectureScheduleService(repository TeacherLectureScheduleRepository, studentLectureScheduleRepo StudentLectureScheduleRepository, token *token.Payload) TeacherLectureScheduleService {
 	res := &teacherLectureScheduleServiceImpl{}
+	token = token
 	res.repo = repository
+	res.studentLectureScheduleRepo = studentLectureScheduleRepo
 	return res
 }
 
@@ -61,4 +69,27 @@ func (c *teacherLectureScheduleServiceImpl) HardDelete(id uint) (*models.Teacher
 
 func (c *teacherLectureScheduleServiceImpl) Restore(id uint) (*models.TeacherLectureSchedule, error) {
 	return c.repo.Restore(id)
+}
+
+func (c *teacherLectureScheduleServiceImpl) UpdateWithStudentLectureSchedule(id uint, data *models.TeacherLectureSchedule) (*models.TeacherLectureSchedule, error) {
+	var err error
+	//scheduleの予約をするのは生徒のみ
+	if c.token.IsTeacher {
+		//TODO:適切なエラーメッセージの設定
+		return nil, err
+	}
+	teacherLectureSchedule, err := c.repo.Update(id, data)
+	if err != nil {
+		return nil, err
+	}
+	_, err = c.studentLectureScheduleRepo.Create(&models.StudentLectureSchedule{
+		StudentID: c.token.ID,
+		StartTime: teacherLectureSchedule.StartTime,
+		EndTime:   teacherLectureSchedule.EndTime,
+		Status:    "pending",
+	})
+	if err != nil {
+		return nil, err
+	}
+	return teacherLectureSchedule, nil
 }
