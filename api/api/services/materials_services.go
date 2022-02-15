63607455f6d3efa2bb9eaad87e6356a66d658b79
KeiTaylor0606 <mvemtae5757@outlook.jp>
2022-02-16 07:44:28 +0900

    講義資料アップロード実装

---
+++ api/api/services/materials_services.go
@@ -0,0 +1,56 @@
+package services
+
+import (
+	"cramee/myerror"
+	"cramee/token"
+	"cramee/util"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/request"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+)
+
+type MaterialsService interface {
+	UploadMaterials(file *os.File, status string, teacherId string, studentId string) error
+}
+
+type materialsServiceImpl struct {
+	config     util.Config
+	tokenMaker token.Maker
+}
+
+func NewMaterialsService(config util.Config, tokenMaker token.Maker) MaterialsService {
+	res := &materialsServiceImpl{}
+	res.config = config
+	res.tokenMaker = tokenMaker
+	return res
+}
+
+func (m *materialsServiceImpl) UploadMaterials(file *os.File, status string, teacherId string, studentId string) error {
+	sess := session.Must(session.NewSession(&aws.Config{
+		Credentials: credentials.NewStaticCredentials(m.config.AwsS3AccessKey, m.config.AwsS3SecretAccessKey, ""),
+		Region:      aws.String(m.config.AwsS3Region),
+	}))
+	uploader := s3manager.NewUploader(sess)
+	key := status + "/" + teacherId + "/" + studentId + "/" + time.Now().Local().Format("2006-01-02--00-00-00")
+	res, err := uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(m.config.AwsS3Bucket),
+		Key:    aws.String(key),
+		Body:   file,
+	})
+	if err != nil {
+		fmt.Println(res)
+		if err, ok := err.(awserr.Error); ok && err.Code() == request.CanceledErrorCode {
+			return myerror.NewPublic(myerror.ErrTimeOut, err)
+		} else {
+			return myerror.NewPublic(myerror.ErrUpload, err)
+		}
+	}
+	return nil
+}
